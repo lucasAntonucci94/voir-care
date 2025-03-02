f7f205e612d702919be5260c73091990f0e328fa
Lucas Antonucci <lucas.e.antonucci@gmail.com>
2025-03-02 16:09:49 -0300

    agrego pinia y corrijo componentes
    ahora se utiliza sidebar de manera que no se genere propagacion de eventos en cadena, sino que se interactua directamente con el store generado para sidebar, centralizando la reactividad de la misma. y obvio permitiendo acceso a ella de forma global, simple y reactiva de donde se edite.

--- src/main.js
+++ src/main.js
@@ -1,9 +1,14 @@
+// main.js
 import './api/firebase'
 import { createApp } from 'vue';
+import { createPinia } from 'pinia';
 import router from './router/router';
 import App from './App.vue';
 import './style.css'
 
-createApp(App)
-.use(router)
-.mount('#app');
+const app = createApp(App);
+const pinia = createPinia();
+
+app.use(pinia); // Integra Pinia en la app
+app.use(router); // Integra el router
+app.mount('#app');
