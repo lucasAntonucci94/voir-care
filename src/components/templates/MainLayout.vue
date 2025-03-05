4ac2b379d2b3943dcb1a5778d25a8ac66832044d
Lucas Antonucci <lucas.e.antonucci@gmail.com>
2025-03-04 23:12:41 -0300

    corrijo estilos y pruebo logica para controlar sidebar (nofunciono)

--- src/components/templates/MainLayout.vue
+++ src/components/templates/MainLayout.vue
@@ -2,11 +2,17 @@
   <div class="flex flex-col min-h-screen">
     <Header />
     <div class="flex flex-1 relative">
-      <!-- Sidebar -->
-      <Sidebar :show="sidebarStore.showSidebar" @toggle="sidebarStore.toggleSidebar" />
+      <!-- Sidebar visible solo en /feed y /profile en desktop, pero siempre accesible en mobile -->
+      <Sidebar
+        :class="{
+          'hidden md:block': !permitedRoutes()
+        }"
+        :show="sidebarStore.showSidebar"
+        @toggle="sidebarStore.toggleSidebar"
+      />
       <!-- Contenido principal -->
       <main class="flex-grow bg-gray-50 font-poppins overflow-y-auto flex-1">
-        <div class="container mx-auto">
+        <div class="container mx-auto px-4">
           <router-view />
         </div>
       </main>
@@ -23,14 +29,14 @@ import { useSidebarStore } from '../../stores/sidebar';
 import { useRoute } from 'vue-router';
 import { useAuth } from '../../api/auth/auth';
 
-const route = useRoute();
+const $route = useRoute();
 const { isAuthenticated } = useAuth();
 const sidebarStore = useSidebarStore();
 
 // Métodos
 const permitedRoutes = () => {
-  var asas = (route.path === '/feed' || route.path === '/profile') && isAuthenticated;
-  return asas
+  console.log(($route.path === '/feed' || $route.path === '/profile') && isAuthenticated)
+  return ($route.path === '/feed' || $route.path === '/profile') && isAuthenticated;
 };
 </script>
 
