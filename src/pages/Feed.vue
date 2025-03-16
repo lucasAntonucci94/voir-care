f185aa5843f79f1a44cc7140440faca53a51f1b4
Lucas Antonucci <lucas.e.antonucci@gmail.com>
2025-03-15 21:48:01 -0300

    corrijo error en suscripciones de posteos
    separo suscripciones a posteos del feed y profile. para evitar solapamiento y sobrecarga al momento de traer los post.

--- src/pages/Feed.vue
+++ src/pages/Feed.vue
@@ -1,10 +1,7 @@
-<!-- Feed.vue -->
 <template>
   <div class="flex flex-1 overflow-hidden">
-    <!-- Contenido principal del Feed (sin Sidebar, ahora en MainLayout) -->
     <main class="flex-grow py-10 bg-gray-50 min-h-screen font-poppins overflow-y-auto">
       <div class="container mx-auto px-4">
-        <!-- Reels o Highlights -->
         <HighlightsCarousel />
         <CreatePostModal />
         <section class="space-y-6 flex flex-col items-center">
@@ -17,7 +14,7 @@
               :key="post.idDoc"
               :post="post"
             />
-            <p v-if="postsStore?.posts?.value?.length === 0" class="text-center text-gray-500">No hay publicaciones aún.</p>
+            <p v-if="postsStore.posts.value.length === 0" class="text-center text-gray-500">No hay publicaciones aún.</p>
           </template>
         </section>
       </div>
@@ -32,20 +29,18 @@ import PostCard from '../components/organisms/PostCard.vue';
 import HighlightsCarousel from '../components/organisms/HighlightsCarousel.vue';
 import { usePostsStore } from '../stores/posts';
 import { useAuth } from '../api/auth/useAuth';
-// Stores y estado
-const { user } = useAuth();
 
+const { user } = useAuth();
 const postsStore = usePostsStore();
 
-// Ciclo de vida
-onMounted(async () => {
-  console.log('Feed.vue montado, iniciando suscripción...');
-  postsStore.subscribe(); // Inicia la suscripción a los posts en tiempo real
+onMounted(() => {
+  console.log('Feed.vue montado, iniciando suscripción global...');
+  postsStore.subscribeGlobal(); // Suscripción global
 });
 
 onUnmounted(() => {
-  console.log('Feed.vue desmontado, cancelando suscripción...');
-  postsStore.unsubscribe(); // Cancela la suscripción al desmontar el componente
+  console.log('Feed.vue desmontado, cancelando suscripción global...');
+  postsStore.unsubscribeGlobal(); // Cancela solo la suscripción global
 });
 </script>
 
